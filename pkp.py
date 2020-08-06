5c5b455b89148f8c4912fb6d6c736ba4b1c2a0a1
Philipp Schmitt <philipp@schmitt.co>
2020-08-06 17:42:33 +0200

    Improve CLI user-experience

--- pkp.py
+++ pkp.py
@@ -16,26 +16,38 @@ def parse_args():
     parser.add_argument("-p", "--password", required=False, help="Password")
     parser.add_argument("-F", "--keyfile", required=False, help="Key file")
     parser.add_argument(
+        "-r",
+        "--raw",
+        action="store_true",
+        default=False,
+        help="Disable REGEX path search",
+    )
+    parser.add_argument(
         "-a", "--attribute", default="password", help="Attribute to fetch"
     )
-    parser.add_argument("path")
+    parser.add_argument("path", default="", nargs="?")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
+    regex = not args.raw
     pkp = pykeepass.PyKeePass(
         filename=args.file, password=args.password, keyfile=args.keyfile
     )
-    entry = pkp.find_entries_by_path(args.path, regex=True)
+    entry = pkp.find_entries_by_path(args.path, regex=regex)
     if not entry:
         print(f"Failed to find entry: {args.path}", file=sys.stderr)
-        group = pkp.find_groups_by_path(os.path.dirname(args.path), regex=True)
+        group = pkp.find_groups_by_path(args.path, regex=regex)
+        if not group:
+            group = pkp.find_groups_by_path(
+                os.path.dirname(args.path), regex=regex
+            )
         if group:
             print("Did you mean one of the following?", file=sys.stderr)
-            for group in group.subgroups:
+            for group in sorted(group.subgroups, key=lambda x: x.path.lower()):
                 print(f"- {group.path}", file=sys.stderr)
-            for entry in group.entries:
+            for entry in sorted(group.entries, key=lambda x: x.path.lower()):
                 print(f"- {entry.path}", file=sys.stderr)
         sys.exit(3)
     print(getattr(entry, args.attribute))
