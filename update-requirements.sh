05882f1c3d851a90c2f996343738d7b78247c03c
Philipp Schmitt <philipp@schmitt.co>
2020-11-03 18:36:39 +0100

    Reorder poetry export args

--- update-requirements.sh
+++ update-requirements.sh
@@ -2,5 +2,5 @@
 
 cd "$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)" || exit 9
 
-poetry export -f requirements.txt -o requirements.txt --without-hashes
-poetry export -f requirements.txt --dev -o requirements-dev.txt --without-hashes
+poetry export -f requirements.txt --without-hashes -o requirements.txt
+poetry export -f requirements.txt --without-hashes --dev -o requirements-dev.txt
