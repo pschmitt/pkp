36941dfb6e65593a8dc2659abb4d2e0db3b5cd6b
Philipp Schmitt <philipp@schmitt.co>
2020-11-04 11:08:25 +0100

    Add version update script

--- dev-update.sh
+++ dev-update.sh
@@ -40,6 +40,57 @@ ${usage_str}
   sed -i "/${marker_start}/r"<(echo "$help_md") README.md
 }
 
+version_compare() {
+  local v1="$1"
+  local v2="$2"
+  local vmax
+
+  vmax=$(echo -e "${v1}\n${v2}" | sort --version-sort | tail -1)
+
+  [[ "$v2" == "$vmax" ]]
+}
+
+update_version() {
+  if [[ -z "$1" ]]
+  then
+    echo "Missing version." >&2
+    return 2
+  fi
+
+  local new_version="$1"
+
+  cd "$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)" || exit 9
+
+  local current_version
+  current_version="$(poetry version -s --no-ansi)"
+
+  if [[ "$new_version" == "bump" ]]
+  then
+    # shellcheck disable=2016
+    new_version="$(sed -r 's/(.+\.)([0-9]+)/echo \1$((\2 + 1))/e' <<< "$current_version")"
+  fi
+
+  if [[ "$current_version" == "$new_version" ]]
+  then
+    echo "We are already on version $new_version" >&2
+    return 3
+  fi
+
+  if [[ -z "$DOWNGRADE" ]]
+  then
+    if ! version_compare "$current_version" "$new_version"
+    then
+      echo "Downgrades are not supported. $current_version > $new_version" >&2
+      return 4
+    fi
+  fi
+
+  echo "🆙 New version: $new_version" >&2
+
+  sed -i -r 's/^(version\s?=\s?)".+"/\1"'"${new_version}"'"/' pyproject.toml
+  sed -i -r 's/^(__version__\s?=\s?)".+"/\1"'"${new_version}"'"/' pkp.py
+}
+
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
 then
   set -euo pipefail
@@ -60,6 +111,11 @@ then
       ;;
     readme|md|README|README.md)
       update_readme
+      ;;
+    version|ver)
+      shift
+      DOWNGRADE="${DOWNGRADE:-}"
+      update_version "$1"
       ;;
     *)
       usage
